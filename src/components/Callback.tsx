b7579f93a30ce673bf2223dcddd054a7c68c5e7d
wbdana <william.b.dana@gmail.com>
2019-04-18 15:13:14 -0400

    POST access_code to back end in callback

--- src/components/Callback.tsx
+++ src/components/Callback.tsx
@@ -5,19 +5,55 @@ export interface IProps {
 }
 
 interface IState {
-
+    accessCode: string;
 }
 
 class Callback extends React.Component<IProps, IState> {
+    constructor(props: IProps) {
+        super(props);
+
+        this.state = {
+            accessCode: "",
+        };
+    }
 
     componentDidMount() {
         console.log('callback! code is:' + window.location.href);
+        this.setState({
+            accessCode: this.getAccessCode(),
+        })
+    }
+
+    getAccessCode = () => {
+        return window.location.href.split("code=")[1];
+    }
+
+    postAccessCode = () => {
+        // This method POSTs the access_code to back end at /auth/convert_token
+        const options = {
+            "method": "POST",
+            "headers": {
+                "content-type": "application/json",
+                "accept": "application/json",
+            },
+            "body": JSON.stringify({
+                code: this.getAccessCode(),
+            }),
+        };
+
+        // fetch("https://github.com/login/oauth/access_token", options)
+        //     .then(resp => console.log(resp));
+
+        fetch("http://localhost:8000/auth/convert_token/", options)
+            .then(resp => console.log(resp));
     }
 
     render() {
         return (
             <div>
-                {window.location.href}
+                We've got a code! It is: {this.getAccessCode()}
+                <br/><br/>
+                <button onClick={this.postAccessCode}>POST access_code</button>
             </div>
         )
     }
