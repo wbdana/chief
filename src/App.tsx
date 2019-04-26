60cb9c5054f0f1cc1ff05106e3881a6b5fc83b33
wbdana <william.b.dana@gmail.com>
2019-04-26 12:23:27 -0400

    Move router switch to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import * as React from 'react';
-import './App.css';
+import { Route, Switch } from 'react-router';
 
-import logo from './logo.svg';
+// Components
+import Callback from './components/Callback';
+import LoginForm from './components/LoginForm';
+
+import './App.css';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Switch>
+        <Route exact path="/" render={() => (<LoginForm />)} />
+        <Route path="/callback" render={() => (<Callback />)} />
+      </Switch>
     );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Route, Switch } from 'react-router';
 import { ConnectedRouter } from 'connected-react-router';
-// import Hello from './containers/Hello';
-// import './index.css';
 
-import LoginForm from './components/LoginForm';
-import Callback from './components/Callback';
+import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-// import { enthusiasm } from './reducers/index';
-// import { EnthusiasmAction } from './actions/index';
-// import { IStoreState } from './types/index';
-
 import configureStore, { history } from './store';
-
-// export const history = createBrowserHistory();
-
-// const store = createStore<IStoreState, EnthusiasmAction, any, any>(enthusiasm, {
-//   enthusiasmLevel: 1,
-//   languageName: 'TypeScript',
-// });
 
 const store = configureStore({
   enthusiasmLevel: 1,
   languageName: 'TypeScript',
 });
 
-
-
-
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Switch>
-      
-        <Route exact path="/" render={() => (<LoginForm />)} />
-        <Route path="/callback" render={() => (<Callback />)} />
-
-      </Switch>
+      <App />
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root') as HTMLElement
